2aa1e1b0a9ca343286c7ce0e5ab731ab6935cd0b
benspector-mls <ben@marcylabschool.org>
2025-01-27 09:35:25 -0500

    updating lecture code

--- 0-lecture-code/lecture.js
+++ 0-lecture-code/lecture.js
@@ -1,4 +1,23 @@
-// Object Oriented Programming encapsulates data with functionality
+/* 
+Functional Programming separates data from functionality
+*/
+const friends = ['ahmad', 'brandon', 'carmen'];
+
+const getUpdatedFriendList = (friends, newFriend) => {
+  const newList = [...friends, newFriend]; // keep it pure, make a new list
+  return newList;
+}
+
+const newFriends = getUpdatedFriendList(friends, 'deema');
+console.log(friends);
+console.log(newFriends);
+
+
+
+
+/* 
+Object Oriented Programming encapsulates data with functionality
+*/
 const friendsManager = {
   friends: [],
   addFriend(newFriend) {
@@ -11,10 +30,7 @@ friendsManager.addFriend('ahmad');
 friendsManager.addFriend('brandon');
 friendsManager.addFriend('carmen');
 // Here, friendsManager is the owner of addFriend. 
-// this.friends === friendsManager.friends
-
-console.log(friendsManager.friends)
-
+// so, this.friends === friendsManager.friends
 
 
 // What about this is NOT consistent or predictable?
@@ -22,10 +38,15 @@ friendsManager.addFriend('daniel');
 friendsManager.addFriend(true);
 friendsManager.friends.push('emmaneul');
 friendsManager.friends.push(42);
-// console.log(friendsManager.friends)
 
 
-// A factory function
+
+
+
+/* 
+A factory function makes and returns an object
+Allows us to use closure.
+*/
 const makeFriendsManager = () => {
 
 }
