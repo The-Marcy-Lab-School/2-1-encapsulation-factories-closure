813e73cc10aaab791c9fcda374fceaa0e07d9d53
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-11-04 10:04:04 -0500

    Refactor 'this' context examples in 3-this.js
    Refactor code to demonstrate the behavior of 'this' in different contexts, including arrow functions and method definitions.

--- 3-this.js
+++ 3-this.js
@@ -1,31 +1,61 @@
-// globally declared variables (without let/const/var) are added to the globalThis object
-firstName = 'Ben';
+// ----------- ARROW FUNCTIONS ------------
+console.log(this); // prints {}
 
-// globalThis has things like setTimeout and setInterval, and now has firstName too
-console.log(globalThis);
-
-// What does `this` reference since when there is no object? The globalThis object!
-function sayMyName() {
-  console.log(this.firstName);
-  console.log(this === globalThis);
+// Arrow function in the global scope
+const arrow = () => {
+  console.log(this);
 }
+// arrow(); // prints {}
 
-sayMyName();
-// Expected Output:
-// Ben
-// true
-
-
-
-function sayMyName() {
-  console.log(this.firstName);
-  console.log(this === globalThis);
+// Arrow Function as a method
+const obj1 = {
+  description: 'obj1',
+  arrow: () => {
+    // the object doesn't create a new scope so the parent scope is still the global scope
+    console.log(this);
+  }
 }
+// obj1.arrow(); // prints {}
 
-const obj = { firstName: 'Ada' };
-obj.sayMyName = sayMyName;
+// Arrow function nested inside another function
+const obj2 = {
+  description: 'obj2',
+  method() {
+    const nested = () => {
+      // the parent scope is now the `obj2.method` function whose `this` value is `obj2`
+      console.log(this);
+    }
+    nested(); // prints obj2
+  }
+}
+// obj2.method();
 
-obj.sayMyName();
-// Expected Output:
-// Ada
-// false
+// ------------- FUNCTION DECLARATIONS/EXPRESSIONS ------------
+console.log(global);
+
+// Function declaration in the global scope
+function functionDeclaration() {
+  console.log(this);
+}
+// functionDeclaration(); // prints the global object
+
+// Function expression in the global scope
+const functionExpression = function() {
+  console.log(this);
+}
+// functionExpression(); // prints the global object
+
+const obj3 = {
+  description: 'obj3',
+  methodDefinition() {
+    // method definition shorthand (equivalent to a function expression below)
+    console.log(this);
+  },
+  methodExpression: function() {
+    // method as a function expression
+    console.log(this);
+  },
+};
+
+// obj3.methodDefinition(); // prints obj3
+// obj3.methodExpression(); // prints obj3
